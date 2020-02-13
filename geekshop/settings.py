b96433c3973504d28592ce7c3901e9a1242491bd
daniltor77 <daniltor77@mail.ru>
2020-02-13 18:41:58 +0300

    не стартует админка

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+SITE_ID = 2
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
@@ -126,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
