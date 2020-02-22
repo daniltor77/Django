4c5c99dced56a6037e64674110b52648bf64424f
daniltor77 <daniltor77@mail.ru>
2020-02-22 23:15:26 +0300

    не отображается количество товаров в корзине на всех страницах, не отображаются изображение товаров в корзине

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
-
+from django.conf import settings
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
 def basket(request):
-    content = {}
+    title= "Корзина"
+    basket_items=Basket.objects.filter(user=request.user).order_by("product__category")
+    content = {"title": title, "basket_items": basket_items, "media_url": settings.MEDIA_URL}
     return render(request, "basketapp/basket.html", content)
 
 
@@ -22,6 +24,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def basket_remove(request):
-    content = {}
-    return render(request, "basketapp/basket.html", content)
+def basket_remove(request, pk):
+    basket_record=get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
